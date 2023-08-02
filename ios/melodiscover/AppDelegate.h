51cf020317794a8efd4832f13b8f8e4db75526fa
Reinhart Andreas <reinhartandreas96@gmail.com>
2023-08-02 19:30:02 +0700

    chore(auth): app-auth integration

--- ios/melodiscover/AppDelegate.h
+++ ios/melodiscover/AppDelegate.h
@@ -1,6 +1,12 @@
 #import <RCTAppDelegate.h>
+#import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
 
-@interface AppDelegate : RCTAppDelegate
+@interface AppDelegate : RCTAppDelegate <RNAppAuthAuthorizationFlowManager>
+
+@property (nonatomic, strong) NSString *moduleName;
+@property (nonatomic, strong) NSDictionary *initialProps;
+@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 
 @end
