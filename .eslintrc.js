dd35dd9acb082ca54277cb575e685b611764fdb1
Reinhart Andreas <reinhartandreas96@gmail.com>
2023-08-03 22:29:45 +0700

    chore(lint): eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,46 @@
 module.exports = {
+  extends: [
+    '@react-native',
+    'prettier',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'sort-destructure-keys',
+    'sort-keys-fix',
+    'typescript-sort-keys',
+  ],
   root: true,
-  extends: '@react-native',
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['warn', 'single'],
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        ignoreCase: false,
+        noSortAlphabetically: false,
+        reservedFirst: ['key'],
+      },
+    ],
+    semi: ['error', 'always'],
+    'sort-destructure-keys/sort-destructure-keys': 'error',
+    'sort-keys': [
+      'warn',
+      'asc',
+      {caseSensitive: true, minKeys: 2, natural: false},
+    ],
+    'sort-keys-fix/sort-keys-fix': 'warn',
+    'typescript-sort-keys/interface': 'error',
+    'typescript-sort-keys/string-enum': 'error',
+  },
 };
