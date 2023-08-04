e99fafc1512f19a9a3a3c1de75d72041c21a6d8e
Reinhart Andreas <reinhartandreas96@gmail.com>
2023-08-04 21:15:21 +0700

    chore(Text): fontFamily props

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,20 @@
 import {createText} from '@shopify/restyle';
 import {Theme} from '../theme/theme';
+import {ComponentProps, PropsWithChildren} from 'react';
 
-const Text = createText<Theme>();
+const RestyleText = createText<Theme>();
+
+export type TextProps = ComponentProps<typeof RestyleText> & {
+  fontFamily?:
+    | 'Montserrat-Bold'
+    | 'Montserrat-ExtraBold'
+    | 'Montserrat-Italic'
+    | 'Montserrat-Medium'
+    | 'Montserrat-SemiBold';
+};
+
+const Text = (props: PropsWithChildren<TextProps>) => {
+  return <RestyleText {...props} />;
+};
 
 export default Text;
