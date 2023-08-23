9f88010dc74be29bdc1830ca99487323312d87f2
Reinhart Andreas <reinhartandreas96@gmail.com>
2023-08-23 20:04:08 +0700

    chore(deps): add gesture-handler, reanimated

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import ThemeProvider from './src/theme/ThemeProvider';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {queryClient} from './src/libs/react-query/queryClient';
 import AppStack from './src/navigation/AppStack';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {StyleSheet} from 'react-native';
 
 const App = () => {
   return (
     <ThemeProvider>
       <SafeAreaProvider>
-        <QueryClientProvider client={queryClient}>
-          <AppStack />
-        </QueryClientProvider>
+        <GestureHandlerRootView style={styles.flex}>
+          <QueryClientProvider client={queryClient}>
+            <AppStack />
+          </QueryClientProvider>
+        </GestureHandlerRootView>
       </SafeAreaProvider>
     </ThemeProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+});
 
 export default App;
